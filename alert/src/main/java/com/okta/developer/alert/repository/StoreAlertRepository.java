0997480f86492823f94f7df7dfd0ab015d108028
kahn <khoa.quocphan@gft.com>
2022-10-19 15:22:47 +0700

    Initial version of alert generated by generator-jhipster@7.9.3

---
+++ alert/src/main/java/com/okta/developer/alert/repository/StoreAlertRepository.java
@@ -0,0 +1,12 @@
+package com.okta.developer.alert.repository;
+
+import com.okta.developer.alert.domain.StoreAlert;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the StoreAlert entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface StoreAlertRepository extends JpaRepository<StoreAlert, Long> {}
